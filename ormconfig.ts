100885d67beb646dbc666ddae82f29f7dbddccf9
dharshan-33 <dharshan@innoview.io>
2024-09-27 10:34:04 +0530

    .ENV file implementation

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,14 +2,17 @@ import { DataSourceOptions } from 'typeorm';
 import { User } from './src/user/entities/user.entity';
 import { Topic } from 'src/user/entities/topic.entity';
 import { Comment } from './src/user/entities/comment.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: DataSourceOptions = {
-  type: 'postgres',
-  database: 'testDB',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
+  type: process.env.DB_TYPE as any,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   entities: [User, Topic, Comment],
   synchronize: true,
 };
